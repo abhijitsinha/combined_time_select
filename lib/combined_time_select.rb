dd640a8a1a411faea2c8e1ec4a5638f28eca5215
Abhijit Sinha <abhijit.sinha@kiprosh.com>
2012-09-21 21:35:43 +0530

    Update lib/combined_time_select.rb

--- lib/combined_time_select.rb
+++ lib/combined_time_select.rb
@@ -7,19 +7,19 @@ end
 module ActionView::Helpers
   class DateTimeSelector
      def select_minute_with_simple_time_select
-        return select_minute_without_simple_time_select unless @options[:combined].eql? true
-
+        return select_minute_without_simple_time_select @options[:combined].eql? true
+        
         # Although this is a datetime select, we only care about the time.  Assume that the date will
         # be set by some other control, and the date represented here will be overriden
-
+        
         val_minutes = @datetime.kind_of?(Time) ? @datetime.min + @datetime.hour*60 : @datetime
-
+        
         @options[:time_separator] = ""
-
+        
         # Default is 15 minute intervals
         minute_interval = 15
-        if @options[:minute_interval]
-          minute_interval = @options[:minute_interval]
+        if @options[:minute_interval] 
+          minute_interval = @options[:minute_interval] 
         end
 
         start_minute = 0
@@ -28,14 +28,14 @@ module ActionView::Helpers
         if @options[:start_hour]
           start_minute = @options[:start_hour] * 60
         end
-
+        
         end_minute = 1439
         # @options[:end_hour] should be specified in military
         # i.e. 0-23
         if @options[:end_hour]
-          end_minute = ( @options[:end_hour] + 1 ) * 60 - 1
+          end_minute = ( @options[:end_hour] + 1 ) * 60 - 1  
         end
-
+        
         if @options[:use_hidden] || @options[:discard_minute]
           build_hidden(:minute, val)
         else
@@ -47,9 +47,9 @@ module ActionView::Helpers
               minute_padded = zero_pad_num(minute%60)
               hour_padded = zero_pad_num(hour)
               ampm_hour = ampm_hour(hour)
-
+              
               val = "#{hour_padded}:#{minute_padded}:00"
-              minute_options << ((val_minutes == minute) ?
+              minute_options << ((val_minutes == minute) ? 
                 %(<option value="#{val}" selected="selected">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n) :
                 %(<option value="#{val}">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n)
               )
@@ -59,43 +59,43 @@ module ActionView::Helpers
         end
       end
       alias_method_chain :select_minute, :simple_time_select
-
-
+      
+      
       def select_hour_with_simple_time_select
         return select_hour_without_simple_time_select unless @options[:combined].eql? true
         # Don't build the hour select
         #build_hidden(:hour, val)
       end
       alias_method_chain :select_hour, :simple_time_select
-
+      
       def select_second_with_simple_time_select
         return select_second_without_simple_time_select unless @options[:combined].eql? true
         # Don't build the seconds select
         #build_hidden(:second, val)
       end
       alias_method_chain :select_second, :simple_time_select
-
+      
       def select_year_with_simple_time_select
         return select_year_without_simple_time_select unless @options[:combined].eql? true
         # Don't build the year select
         #build_hidden(:year, val)
       end
       alias_method_chain :select_year, :simple_time_select
-
+      
       def select_month_with_simple_time_select
         return select_month_without_simple_time_select unless @options[:combined].eql? true
         # Don't build the month select
         #build_hidden(:month, val)
       end
       alias_method_chain :select_month, :simple_time_select
-
+      
       def select_day_with_simple_time_select
         return select_day_without_simple_time_select unless @options[:combined].eql? true
         # Don't build the day select
         #build_hidden(:day, val)
       end
       alias_method_chain :select_day, :simple_time_select
-
+     
   end
 end
 
